b6cf7f73fc465a95f562ad74c4843e752c8b93fc
lynfgn <707557870@qq.com>
2020-03-08 00:30:09 +0800

    长整数相加demo

---
+++ src/main/java/temp/DemoLongIntegerAdd.java
@@ -0,0 +1,22 @@
+package temp;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 超长整数相加
+ */
+public class DemoLongIntegerAdd {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            String s1 = input.next();
+            String s2 = input.next();
+            BigInteger num1 = new BigInteger(s1);//采用BigInteger可以直接进行大整数进行计算
+            BigInteger num2 = new BigInteger(s2);
+            System.out.println(num1.add(num2));
+        }
+    }
+
+}
